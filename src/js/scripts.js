1d0325de7b252fc00ebf8646ea1a50357e5208ee
Joe Karasek <joseph.karasek@gmail.com>
2017-02-27 09:02:38 -0800

    update insurance, cta, reviews, and specials

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,25 +1,25 @@
-(function($, HelloWorldDevsTysonSteele) {
+(function($, HWD) {
 
   // ======= Fixes =======
-  HelloWorldDevsTysonSteele.noOrphans('h1,h2,h3,h4,h5,h6,li,p', '.allow-orphan');
-  HelloWorldDevsTysonSteele.scrollToFix('#primary-menu');
-  HelloWorldDevsTysonSteele.stopVideoModal('#videoModal', '#video-one');
+  HWD.noOrphans('h1,h2,h3,h4,h5,h6,li,p', '.allow-orphan');
+  HWD.scrollToFix('#primary-menu');
+  HWD.stopVideoModal('#videoModal', '#video-one');
 
   // ======= Request Appointment Form =======
-  HelloWorldDevsTysonSteele.requestAppointment('#mail-form', '#success_msg' , '7fb35345-752d-4792-9480-cd3db6674a62');
+  HWD.requestAppointment('#mail-form', '#success_msg' , '7fb35345-752d-4792-9480-cd3db6674a62');
 
   // ======= Google Maps =======
-  HelloWorldDevsTysonSteele.googleMap('#google-map5', '33.9527789,-84.337107', '33.9502465,-84.3370638');
+  HWD.googleMap('#google-map5', '33.9527789,-84.337107', '33.9502465,-84.3370638');
 
   // ======= Carousels =======
-  HelloWorldDevsTysonSteele.marqueeCarousel({ autoplay: false });
-  HelloWorldDevsTysonSteele.itemCarousel('.js-services-carousel', {
+  HWD.marqueeCarousel({ autoplay: false });
+  HWD.itemCarousel('.js-services-carousel', {
     navText: [
-      '< img src="assets/carousel-arrow.png" alt="navigation arrow">',
+      '<img src="assets/carousel-arrow.png" alt="navigation arrow">',
       '<img src="assets/carousel-arrow.png" alt="navigation arrow">'
     ]
   });
-  HelloWorldDevsTysonSteele.itemCarousel('.js-team-carousel', {
+  HWD.itemCarousel('.js-team-carousel', {
     nav: false,
   });
 
